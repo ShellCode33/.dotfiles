60b756d217590af892ff10c420c33ad94234725f
ShellCode33 <shellcode33@proton.me>
2023-08-15 06:15:01 +0200

    nvim: disable default <c-k> keymap

--- .config/nvim/lua/plugins/disabled-keymaps.lua
+++ .config/nvim/lua/plugins/disabled-keymaps.lua
@@ -4,14 +4,7 @@ return {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-      for index, keymap in pairs(keys) do
-        -- Remove <c-k> keymap in insert mode because it conflicts with our own
-        if keymap[1] == "<c-k>" and keymap.mode == "i" then
-          keys[index] = nil
-          break
-        end
-      end
+      keys[#keys + 1] = { "<c-k>", false, mode = "i" }
     end,
   },
 }
