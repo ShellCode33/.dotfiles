2bbafaac695cfd49f581e99538ef2b4f238af1af
ShellCode33 <shellcode33@proton.me>
2023-10-22 19:08:36 +0200

    nvim: add dashboard colors to the GruvBox theme

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -10,10 +10,18 @@ return {
         TelescopePreviewTitle = { fg = "#282828", bg = "#d65d0e" },
         TelescopeTitle = { fg = "#282828", bg = "#98971a" },
 
-        -- Highlight what's under cursor
-        IlluminatedWordText = { bg = "#333333" },
-        IlluminatedWordRead = { bg = "#333333" },
-        IlluminatedWordWrite = { bg = "#333333" },
+        -- Highlight symbol under cursor
+        IlluminatedWordText = { bg = "#333333", bold = true },
+        IlluminatedWordRead = { bg = "#333333", bold = true },
+        IlluminatedWordWrite = { bg = "#333333", bold = true },
+
+        -- Dashboard colors
+        DashboardHeader = { link = "GruvboxYellow" },
+        DashboardFooter = { link = "GruvboxYellow" },
+        DashboardDesc = { link = "GruvboxGray" },
+        DashboardKey = { link = "GruvboxYellow" },
+        DashboardIcon = { link = "GruvboxYellow" },
+        DashboardShortCut = { link = "GruvboxRed" },
       },
     },
   },
