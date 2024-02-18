4e95a41d225a78a3af826d444c7938d45b9fc3ff
ShellCode33 <shellcode33@proton.me>
2024-02-18 09:24:14 +0100

    nvim: minor pairs improvement

--- .config/nvim/lua/plugins/pairs.lua
+++ .config/nvim/lua/plugins/pairs.lua
@@ -20,7 +20,7 @@ return {
           ["]"] = { action = "close", pair = "[]", neigh_pattern = ".." },
           ["}"] = { action = "close", pair = "{}", neigh_pattern = ".." },
 
-          ["<"] = { action = "open", pair = "<>", neigh_pattern = ".[^%a]", register = { cr = false } },
+          ["<"] = { action = "open", pair = "<>", neigh_pattern = "[%a][^%a]", register = { cr = false } },
           [">"] = { action = "close", pair = "<>", neigh_pattern = ".[^%a]", register = { cr = false } },
 
           ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^%a\\][^%a]", register = { cr = false } },
